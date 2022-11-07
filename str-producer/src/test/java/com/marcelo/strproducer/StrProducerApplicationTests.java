900ea10836471362ee0ca7cfc6d6ac08560c39a6
Marcelo Moura <marcelohmoura99@outlook.com.br>
2022-11-07 07:43:51 -0300

    Criando projeto de produtor de Strings

---
+++ str-producer/src/test/java/com/marcelo/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
